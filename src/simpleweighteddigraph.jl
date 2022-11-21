c5b2cdd3a63b46fc3404f3e8cb328b3144de60d7
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2022-11-21 07:19:22 -0500

    Update src/simpleweighteddigraph.jl
    Co-authored-by: Simon Schölly <sischoel@gmail.com>

--- src/simpleweighteddigraph.jl
+++ src/simpleweighteddigraph.jl
@@ -120,7 +120,7 @@ end
 rem_edge!(g::SimpleWeightedDiGraph, e::AbstractEdge) =
     rem_edge!(g, src(e), dst(e))
 
-function rem_edge!(g::SimpleWeightedDiGraph{T, U}, u::Integer, v::Integer) where {T<:Integer, U<:Real}
+function rem_edge!(g::SimpleWeightedDiGraph, u::Integer, v::Integer)
     (u ∈ vertices(g) && v ∈ vertices(g)) || return false
     w = g.weights
     indx = _get_nz_index!(w, v, u) # get the index in nzval
