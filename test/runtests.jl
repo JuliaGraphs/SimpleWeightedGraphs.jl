25b9d91f8f20bf1105e1ab483e1541dd8d4d8b2f
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-03-09 11:38:15 +0100

    Move docs from readme to website, better testing (#34)
    - Use Documenter to create a documentation website featuring the
    docstrings and former README tutorial
    - Add this build step to CI
    - Test for code quality and formatting (indicated as broken because I
    didn't want to overload this PR with formatting changes)
    
    ---------
    
    Co-authored-by: Mathieu Besançon <mathieu.besancon@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,25 @@
+using Aqua
+using Documenter
 using Graphs
-using SimpleWeightedGraphs
+using JuliaFormatter
 using LinearAlgebra
+using SimpleWeightedGraphs
 using Test
+
+DocMeta.setdocmeta!(
+    SimpleWeightedGraphs, :DocTestSetup, :(using SimpleWeightedGraphs); recursive=true
+)
 
 testdir = dirname(@__FILE__)
 
-testgraphs(g) = [g, SimpleWeightedGraph{UInt8,Float64}(g), SimpleWeightedGraph{Int16,Float32}(g)]
-testdigraphs(g) = [g, SimpleWeightedDiGraph{UInt8,Float64}(g), SimpleWeightedDiGraph{Int16,Float32}(g)]
+function testgraphs(g)
+    return [g, SimpleWeightedGraph{UInt8,Float64}(g), SimpleWeightedGraph{Int16,Float32}(g)]
+end
+function testdigraphs(g)
+    return [
+        g, SimpleWeightedDiGraph{UInt8,Float64}(g), SimpleWeightedDiGraph{Int16,Float32}(g)
+    ]
+end
 
 testsimplegraphs(g) = [g, Graphs.SimpleGraph{UInt8}(g), Graphs.SimpleGraph{Int16}(g)]
 testsimpledigraphs(g) = [g, Graphs.SimpleDiGraph{UInt8}(g), Graphs.SimpleDiGraph{Int16}(g)]
@@ -17,10 +30,20 @@ tests = [
     "overrides",
     "persistence",
     "connectivity",
-    "a_star"
+    "a_star",
 ]
 
 @testset verbose = true "SimpleWeightedGraphs" begin
+    @testset verbose = true "Code quality (Aqua.jl)" begin
+        Aqua.test_all(SimpleWeightedGraphs; ambiguities=false)
+    end
+    @testset verbose = false "Code formatting (JuliaFormatter.jl)" begin
+        @test_broken format(SimpleWeightedGraphs; verbose=false, overwrite=false)
+    end
+    @testset verbose = false "Doctests (Documenter.jl)" begin
+        doctest(SimpleWeightedGraphs)
+    end
+
     for t in tests
         tp = joinpath(testdir, "$(t).jl")
         include(tp)
